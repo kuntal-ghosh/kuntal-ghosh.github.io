06c8f635f0504d80b8222bff6627f60564a67239
kuntalghosh <kuntalghosh.ewu@gmail.com>
2020-08-15 14:59:24 +0600

    Updates

---
+++ precache-manifest.abd5ab45c46e23ae754da89f43283e92.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83803ad5ff79806fe82eb9a129534f7d",
+    "url": "/kuntal711.github.io/index.html"
+  },
+  {
+    "revision": "6b63d8aaf7508990183a",
+    "url": "/kuntal711.github.io/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "ebb080924ba827469ea7",
+    "url": "/kuntal711.github.io/static/css/main.e84913fb.chunk.css"
+  },
+  {
+    "revision": "6b63d8aaf7508990183a",
+    "url": "/kuntal711.github.io/static/js/2.6dc7c2dc.chunk.js"
+  },
+  {
+    "revision": "effe4e92e8658ad9dfbf53e804a021fb",
+    "url": "/kuntal711.github.io/static/js/2.6dc7c2dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebb080924ba827469ea7",
+    "url": "/kuntal711.github.io/static/js/main.6094f7da.chunk.js"
+  },
+  {
+    "revision": "d0c0f88b04cc541bf1bf",
+    "url": "/kuntal711.github.io/static/js/runtime-main.b14a0578.js"
+  },
+  {
+    "revision": "284dade4716e67ab8b797ce4c8643e8e",
+    "url": "/kuntal711.github.io/static/media/profile.284dade4.jpg"
+  }
+]);
